24eb57a84ceb5de54638c275628c8490cb53c2a9
Stefke Velja <stef.fac150@gmail.com>
2024-04-29 22:30:47 +0200

    Initial commit

---
+++ src/main/java/fon/basketball/analyzer/DemoApplication.java
@@ -0,0 +1,16 @@
+package fon.basketball.analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
