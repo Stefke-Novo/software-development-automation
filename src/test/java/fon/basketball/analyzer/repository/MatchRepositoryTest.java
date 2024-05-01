4f2d6d5bd0bacf26a044d13b4c06a9ddcf5d9c16
Stefke Velja <stef.fac150@gmail.com>
2024-05-01 23:39:54 +0200

    Changed test with mocked instance

--- src/test/java/fon/basketball/analyzer/repository/MatchRepositoryTest.java
+++ src/test/java/fon/basketball/analyzer/repository/MatchRepositoryTest.java
@@ -2,26 +2,40 @@ package fon.basketball.analyzer.repository;
 
 import fon.basketball.analyzer.models.Match;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.util.Assert.isInstanceOf;
 
 
 @SpringBootTest
 class MatchRepositoryTest {
 
-    @Autowired
-    MatchRepository matchRepository;
+//    @Autowired
+//    MatchRepository matchRepository;
+
+    @Mock
+    MatchRepository matchRepository1;
+
+//    @Test
+//    public void testMatchList(){
+//
+//        List<Match> matches = matchRepository.findAll();
+//        isInstanceOf(ArrayList.class,
+//                matches);
+//    }
 
     @Test
-    public void testMatchList(){
-
-        List<Match> matches = matchRepository.findAll();
+    public void testMatchListMocked(){
+        when(matchRepository1.findAll()).thenReturn(new ArrayList<>(Arrays.asList(new Match(),new Match())));
+        List<Match> matches = matchRepository1.findAll();
         isInstanceOf(ArrayList.class,
                 matches);
     }
